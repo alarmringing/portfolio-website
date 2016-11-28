16f9c336c3f50f9f15e9a36da0c5e93ab1d92b70
Jihee Hwang <jiheeh@stanford.edu>
2016-11-27 23:16:33 -0800

    Changed intro

--- app/about.jsx
+++ app/about.jsx
@@ -15,8 +15,8 @@ export default class About extends React.Component {
     		  <p>
     			 I'm a programmer, designer, audiophile, singer, artist, and a tea connoisseur. Born and raised in Seoul, South Korea, I've always enjoyed being a bit of everything. 
     		  </p>
-  			 <p>I was a gamer since young, but my discovery of indie games brought me to realize the potential of a game as an interactive medium of expression and creativity. This led  to an interest in computer science,&nbsp;which I am pursuing as a major in Stanford right now. Currently my biggest interests lie in Virtual Reality, AI and Neural Computation. </p>
-  			 <p>I also happen to design and paint things. What started out from doodling in corners of textbooks ended up as passion in visual art. I believe visual art empowers and contextualizes what is otherwise just a banal piece of material. I try to make my designs nonconforming but inspired by diverse artistic movements at the same time. I also paint digitally and acrylic is my preferred non-digital paint.</p>  
+  			 <p>I was a gamer since young, but my discovery of indie games brought me to realize the potential of a game as an interactive medium of expression and creativity. This led to an interest in computer science,&nbsp;which I am pursuing as a major in Stanford right now. Also dabbling in Linguistics and Japanese as my minors, every day I find myself more and more captivated by linguistics and syntax, and the power of language to encode information with incredible consistenty. Currently some of my biggest interests lie in Virtual Reality, AI, NLP research and Neural Computation. </p>
+  			 <p>I also happen to design and paint things. What started out from doodling in corners of textbooks ended up as passion in visual art. Visual design is not separable from anything else I do in my life, but rather seeps into every corner of it, in my web applications, games and fashion. I also paint digitally while acrylic is my preferred non-digital paint.</p>  
         </div>
         <div id = "buttons" style = {{textAlign: 'left'}}>
           <a style = {buttonStyle} href="https://github.com/alarmringing"> 
