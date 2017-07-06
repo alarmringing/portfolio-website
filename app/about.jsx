48801ed528aa2e9c5792d5a087e3ac705b326d27
Jihee Hwang <jiheeh@stanford.edu>
2017-07-06 08:01:23 -0700

    Changed about page

--- app/about.jsx
+++ app/about.jsx
@@ -13,10 +13,10 @@ export default class About extends React.Component {
         <div id="filler" style = {{height: 100}} />
         <div style = {{textAlign: 'left'}}>
     		  <p>
-    			 I'm a programmer, designer, audiophile, singer, artist, and a tea connoisseur. Born and raised in Seoul, South Korea, I've always enjoyed being a bit of everything. 
+    			 I'm a programmer, designer, audiophile, singer, and a tea connoisseur. Born and raised in Seoul, South Korea, I've always enjoyed being a bit of everything. 
     		  </p>
-  			 <p>I was a gamer since young, but my discovery of indie games brought me to realize the potential of a game as an interactive medium of expression and creativity. This led to an interest in computer science,&nbsp;which I am pursuing as a major in Stanford right now. Also dabbling in Linguistics and Japanese as my minors, every day I find myself more and more captivated by linguistics and syntax, and the power of language to encode information with incredible consistenty. Currently some of my biggest interests lie in Virtual Reality, AI, NLP research and Neural Computation. </p>
-  			 <p>I also happen to design and paint things. What started out from doodling in corners of textbooks ended up as passion in visual art. Visual design is not separable from anything else I do in my life, but rather seeps into every corner of it, in my web applications, games and fashion. I also paint digitally while acrylic is my preferred non-digital paint.</p>  
+  			 <p>I was a gamer since young, but my discovery of indie games brought me to realize the potential of a game as an interactive medium of expression and creativity. This led to an interest in computer science,&nbsp;which I am pursuing as a major in Stanford right now. Also dabbling in Linguistics and Japanese as my minors, some of my biggest interests lie in Virtual Reality, AI, NLP and Computer Vision. </p>
+  			 <p>It also happens that I'm slightly <i>artsy</i>. It shows through sometimes.</p>  
         </div>
         <div id = "buttons" style = {{textAlign: 'left'}}>
           <a style = {buttonStyle} href="https://github.com/alarmringing"> 
